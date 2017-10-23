ae1ab8027fac53f7fc26018830f19cd115fe9ec3
DS-Ubuntu <96lives@gmail.com>
2017-10-23 19:27:17 +0900

    first db, added classes

---
+++ ynm/db/tests.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+# Create your tests here.
+
+class Room(models.Model):
+	title = models.CharField(max_length=64)
+
+class InRoom(models.Model):
+	
+
+class FreeTime(models.Model):
+
+class TimeSlot(models.Model):
+
+ 
+
+
+
+
