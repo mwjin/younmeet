a9d7462ef19501d33c1ff2d248ba8cc8d5ab7e57
ktaebum <ktaebum@gmail.com>
2017-11-02 00:54:30 +0900

    add user object & account service basic & login component skeleton

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations : [
+    AppComponent,
+    LoginComponent
   ],
-  imports: [
+  imports : [
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers : [],
+  bootstrap : [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+}
