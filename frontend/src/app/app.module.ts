94eed17d85d9eac86e0827b56fccb08e0ce35507
ktaebum <ktaebum@gmail.com>
2017-11-03 13:52:34 +0900

    add angular2-semantic-ui integration

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SuiModule } from 'ng2-semantic-ui';
 
 @NgModule({
-  declarations: [
+  declarations : [
     AppComponent
   ],
-  imports: [
-    BrowserModule
+  imports : [
+    BrowserModule,
+    SuiModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers : [],
+  bootstrap : [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
