37996cc751c85e0c78db6b431ce0566d41ec37a4
ktaebum <ktaebum@gmail.com>
2017-11-03 14:09:14 +0900

    add login routing, modify app module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
 import { SuiModule } from 'ng2-semantic-ui';
+import { YounmeetRoutingModule } from './younmeet-routing/younmeet-routing.module';
+import { HttpModule } from '@angular/http';
+import { AccountService } from './services/account.service';
+
+import { APP_BASE_HREF } from '@angular/common';
 
 @NgModule({
   declarations : [ AppComponent, LoginComponent ],
-  imports : [ BrowserModule, SuiModule ],
-  providers : [],
+  imports : [ BrowserModule, SuiModule, YounmeetRoutingModule, HttpModule ],
+  providers : [ AccountService,
+    { provide : APP_BASE_HREF, useValue : '/' } ],
   bootstrap : [ AppComponent ]
 })
 export class AppModule {
--- frontend/src/app/younmeet-routing/younmeet-routing.module.ts
+++ frontend/src/app/younmeet-routing/younmeet-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from '../login/login.component';
+import { RouterModule, Routes } from '@angular/router';
+
+export const routes: Routes = [
+  { path : '', redirectTo : '/login', pathMatch : 'full' },
+  { path : 'login', component : LoginComponent }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
+  imports : [
+    CommonModule,
+    RouterModule.forRoot(routes)
   ],
-  declarations: []
+  declarations : [],
+  exports : [ RouterModule ]
 })
-export class YounmeetRoutingModule { }
+export class YounmeetRoutingModule {}
