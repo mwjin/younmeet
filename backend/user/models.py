0f9e93cd774a24c66ab2be6818cece6ae495bda5
JeongMW <jmw95217@gmail.com>
2017-11-03 22:26:04 +0900

    modify models (add google_account)

--- backend/user/models.py
+++ backend/user/models.py
@@ -5,6 +5,7 @@ from django.utils.translation import ugettext_lazy as _
 class User(AbstractUser):
     username = None
     email = models.EmailField(_('email address'), unique=True)
+    google_account = models.EmailField(blank=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotAllowed, HttpResponseForbidden
+from django.contrib.auth import authenticate, login, logout
+from models import User
+import json
 
-# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        req_data = json.loads(request.body.decode())
+        email = req_data['email']
+        password = req_data['password']
+        google_account = req_data['google_account']
+        User.objects.create_user(email=email, password=password, google_account=google_account)
+        return HttpResponse(status=201)
+    else:
+        return HttpResponseNotAllowed(['POST'])
+
+
+def signin(request):
+    if request.method == 'POST':
+        req_data = json.loads(request.body.decode())
+        username = req_data['username']
+        password = req_data['password']
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponse(status=200)
+        else:
+            return HttpResponse(status=401)  # Unauthorized user
+
+    else:
+        return HttpResponseNotAllowed(['POST'])
+
+
+def signout(request):
+    if request.method == 'GET':
+        logout(request)
+        return HttpResponse(status=200)
+    else:
+        return HttpResponseNotAllowed(['GET'])
