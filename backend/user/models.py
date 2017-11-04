8974fc697cb3819add07e3bfdbdb75bf1a9e3d04
JeongMW <jmw95217@gmail.com>
2017-11-04 14:20:06 +0900

    alter migration

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,11 +1,18 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.utils.translation import ugettext_lazy as _
+
+
+class MyUserManager(BaseUserManager):
+    """
+    This custom is intended to deal with emails as unique identifiers for auth instead of username.
+    """
+    pass
 
 class User(AbstractUser):
     username = None
     email = models.EmailField(_('email address'), unique=True)
-    google_account = models.EmailField(blank=True)
+    google_account = models.EmailField(_('google account'), blank=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
