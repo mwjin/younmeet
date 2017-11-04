9080269c6f7321a27932e5addae2a0d2295af8ad
KIM TAE BUM <phya.ktaebum@gmail.com>
2017-11-04 17:14:59 +0900

    started to make authentication test

--- frontend/src/app/services/authentication.service.spec.ts
+++ frontend/src/app/services/authentication.service.spec.ts
@@ -1,15 +1,81 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject, async } from '@angular/core/testing';
+import {
+  HttpModule,
+  Http,
+  Response,
+  ResponseOptions,
+  XHRBackend
+} from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
 
 import { AuthenticationService } from './authentication.service';
+import { User } from '../models/user';
+
+const makeMockUserDB = () => [
+  {
+    id : 1,
+    username : 'user1',
+    email : 'swpp2017@gmail.com',
+    password : 'iluvswpp'
+  },
+  {
+    id : 2,
+    username : 'user2',
+    email : 'swpp2016@naver.com',
+    password : 'ihateswpp'
+  }
+] as User[];
+
 
 describe('AuthenticationService', () => {
   beforeEach(() => {
+    const local = {};
+
+
+    spyOn(localStorage, 'getItem').and.callFake((key: string): String => {
+      return local[ key ] || null;
+    });
+
+    spyOn(localStorage, 'setItem').and.callFake((key: string, value: string): string => {
+      return local[ key ] = <string> value;
+    });
+
+    spyOn(localStorage, 'removeItem').and.callFake((key: string): void => {
+      delete local[ key ];
+    });
+
     TestBed.configureTestingModule({
-      providers: [AuthenticationService]
+      imports : [ HttpModule ],
+      providers : [ AuthenticationService,
+        { provide : XHRBackend, useClass : MockBackend } ]
     });
   });
 
-  it('should be created', inject([AuthenticationService], (service: AuthenticationService) => {
-    expect(service).toBeTruthy();
+  it('can make object with "new"', inject([ Http ], (http: Http) => {
+    expect(http).not.toBeNull('http should be provided');
+    let service = new AuthenticationService(http);
+    expect(service instanceof AuthenticationService).toBe(true);
   }));
+
+  it('local storage currentUser should be empty before sign in', inject([ Http ], (http: Http) => {
+    expect(localStorage.getItem[ 'currentUser' ]).toBeNull();
+  }));
+
+  /*
+  describe('When signin', () => {
+    let backend: MockBackend;
+    let service: AuthenticationService;
+
+    let fakeUsers: User[];
+    let response: Response;
+
+    beforeEach(inject([ Http, XHRBackend ], (http: Http, be: MockBackend) => {
+      backend = be;
+      service = new AuthenticationService(http);
+
+      fakeUsers = makeMockUserDB();
+
+    }));
+  });
+  */
 });
