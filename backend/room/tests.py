7b026ff0403f14e6569c70673ef853fef71e05ad
DS-Ubuntu <96lives@gmail.com>
2017-11-05 11:54:48 +0900

    add Testing. statement test: 100%  branch test:100%

--- backend/room/tests.py
+++ backend/room/tests.py
@@ -1,6 +1,5 @@
 from django.test import TestCase, Client
 from .models import Room
-from django.forms.models import model_to_dict
 from datetime import datetime
 from django.utils import timezone
 from user.models import User
@@ -8,19 +7,22 @@ import json
 
 CONTENT_TYPE = 'application/json'
 
+
 class RoomTestCase(TestCase):
 
     def setUp(self):
         User.objects.create_user(email='email1', password='password1')
         User.objects.create_user(email='email2', password='password2')
         User.objects.create_user(email='email3', password='password3')
+        User.objects.create_user(email='email4', password='password4')
         user1 = User.objects.get(id=1)
         user2 = User.objects.get(id=2)
+        user3 = User.objects.get(id=3)
+        user4 = User.objects.get(id=4)
 
-
+        #timezone.make_aware() is used to suppress warning
         min_time1 = timezone.make_aware(datetime.strptime('2000-01-1 2:30', '%Y-%m-%d %H:%M'))
         min_time2 = timezone.make_aware(datetime.strptime('2000-01-1 1:00', '%Y-%m-%d %H:%M'))
-        create_time = timezone.localtime(timezone.now())
 
         best_start_time1 = timezone.make_aware(datetime.strptime('2017-11-4 12:30', '%Y-%m-%d %H:%M'))
         best_end_time1 = timezone.make_aware(datetime.strptime('2017-11-4 17:30', '%Y-%m-%d %H:%M'))
@@ -47,26 +49,139 @@ class RoomTestCase(TestCase):
         room1 = Room.objects.get(id=1)
         room2 = Room.objects.get(id=2)
         room1.members.add(user1)
+        room1.members.add(user2)
+        room1.members.add(user3)
+        room1.members.add(user4)
         room2.members.add(user1)
 
         self.client = Client()
 
-
     def test_room_list_get(self):
-        self.client.post('/api/signin',
-                         json.dumps({'email': 'email1', 'password': 'password1'}),
-                         content_type=CONTENT_TYPE)
+        self.client.post(
+            '/api/signin',
+            json.dumps({'email': 'email1', 'password': 'password1'}),
+            content_type=CONTENT_TYPE
+        )
         response = self.client.get('/api/rooms')
+        self.assertEqual(response.status_code, 200)
         data = json.loads(response.content.decode())
         self.assertEqual(len(data), 2)
         self.assertEqual(data[0]['name'], 'room1')
         self.assertEqual(data[1]['place'], 'place2')
 
-'''
     def test_room_list_post(self):
-        self.client.post('/api/signin',
-                         json.dumps({'username': 'user1', 'password': 'password1'}),
-                         content_type=CONTENT_TYPE)
-        response = self.client.post('/api/rooms', json.dump)
+        self.client.post(
+            '/api/signin',
+            json.dumps({'email': 'email1', 'password': 'password1'}),
+            content_type=CONTENT_TYPE
+        )
+        min_time = timezone.make_aware(datetime.strptime('2000-01-1 1:00', '%Y-%m-%d %H:%M'))
+        response = self.client.post(
+            '/api/rooms',
+            json.dumps({'name': 'room1',
+                        'place': 'place1',
+                        'min_time_required': str(min_time),
+                        }),
+            content_type=CONTENT_TYPE,
+        )
+        self.assertEqual(response.status_code, 201)
+        response = self.client.get('/api/rooms')
+        data = json.loads(response.content.decode())
+        self.assertEqual(len(data), 3)
 
-'''
+    def test_room_list_delete(self):
+        self.client.post(
+            '/api/signin',
+            json.dumps({'email': 'email1', 'password': 'password1'}),
+            content_type=CONTENT_TYPE
+        )
+        response = self.client.delete('/api/rooms')
+        self.assertEqual(response.status_code, 405)
+
+    def test_room_list_get_not_authenticated(self):
+        response = self.client.get('/api/rooms')
+        self.assertEqual(response.status_code, 401)
+
+    def test_room_detail_get(self):
+        self.client.post(
+            '/api/signin',
+            json.dumps({'email': 'email1', 'password': 'password1'}),
+            content_type=CONTENT_TYPE
+        )
+        response = self.client.get('/api/rooms/1')
+        self.assertEqual(response.status_code, 200)
+        room = json.loads(response.content.decode())
+        self.assertEqual(room['name'], 'room1')
+        self.assertEqual(room['place'], 'place1')
+
+    def test_room_detail_delete(self):
+        self.client.post(
+            '/api/signin',
+            json.dumps({'email': 'email1', 'password': 'password1'}),
+            content_type=CONTENT_TYPE
+        )
+        response = self.client.delete('/api/rooms/1')
+        self.assertEqual(response.status_code, 204)
+        response = self.client.get('/api/rooms')
+        data = json.loads(response.content.decode())
+        self.assertEqual(len(data), 1)
+
+    def test_room_detail_post(self):
+        self.client.post(
+            '/api/signin',
+            json.dumps({'email': 'email1', 'password': 'password1'}),
+            content_type=CONTENT_TYPE
+        )
+        response = self.client.post('/api/rooms/1')
+        self.assertEqual(response.status_code, 405)
+
+    def test_room_detail_get_not_authenticated(self):
+        response = self.client.get('/api/rooms/1')
+        self.assertEqual(response.status_code, 401)
+
+    def test_room_detail_get_not_found(self):
+        self.client.post(
+            '/api/signin',
+            json.dumps({'email': 'email1', 'password': 'password1'}),
+            content_type=CONTENT_TYPE
+        )
+        response = self.client.get('/api/rooms/100')
+        self.assertEqual(response.status_code, 404)
+
+    def test_room_members_get(self):
+        self.client.post(
+            '/api/signin',
+            json.dumps({'email': 'email1', 'password': 'password1'}),
+            content_type=CONTENT_TYPE
+        )
+        response = self.client.get('/api/rooms/1/members')
+        data = json.loads(response.content.decode())
+        self.assertEqual(len(data), 4)
+
+    def test_room_members_post(self):
+        self.client.post(
+            '/api/signin',
+            json.dumps({'email': 'email1', 'password': 'password1'}),
+            content_type=CONTENT_TYPE
+        )
+        response = self.client.post('/api/rooms/1/members')
+        self.assertEqual(response.status_code, 405)
+
+    def test_room_members_get_not_authenticated(self):
+        response = self.client.get('/api/rooms/1/members')
+        self.assertEqual(response.status_code, 401)
+
+    def test_room_members_get_not_found(self):
+        self.client.post(
+            '/api/signin',
+            json.dumps({'email': 'email1', 'password': 'password1'}),
+            content_type=CONTENT_TYPE
+        )
+        response = self.client.get('/api/rooms/100/members')
+        self.assertEqual(response.status_code, 404)
+
+
+
+
+
+
--- backend/room/views.py
+++ backend/room/views.py
@@ -1,56 +1,55 @@
-from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotAllowed
 from django.http import HttpResponseNotFound, JsonResponse
 from django.forms.models import model_to_dict
-from datetime import datetime
 from .models import Room
 import json
 
 
-
 def room_list(request):
+
     if not request.user.is_authenticated():
         return HttpResponse(status=401)
     user = request.user
 
     if request.method == 'GET':
-        return JsonResponse(list(Room.objects.filter(members__id=user.id).values()), safe=False)
+        return JsonResponse(list(Room.objects.all().values()), safe=False)
+        # return JsonResponse(list(Room.objects.filter(members__id=user.id).values()), safe=False)
 
     elif request.method == 'POST':
         data = json.loads(request.body.decode())
         name = data['name']
         place = data['place']
         min_time_required = data['min_time_required']
-        created_time = datetime.now()
         new_room = Room(
             name=name,
             place=place,
             min_time_required=min_time_required,
-            created_time=created_time,
             owner=user
         )
         new_room.save()
         # does not add this user to new_room.users
         # room.user is only added when selecting free_time
-        return HttpResponse( status=201)
+        return HttpResponse(status=201)
 
     else:
         return HttpResponseNotAllowed(['GET', 'POST'])
 
 
 def room_detail(request, room_id):
-
     if not request.user.is_authenticated():
         return HttpResponse(status=401)
 
-    room_id = int(room_id)
     try:
         room = Room.objects.get(id=room_id)
     except Room.DoesNotExist:
         return HttpResponseNotFound()
 
+    '''  
+    TODO: Object of type 'User' is not JSON serializable
+    members of the room are currently excluded
+    '''
     if request.method == 'GET':
-        return JsonResponse(model_to_dict(room))
+        return JsonResponse(model_to_dict(room, exclude='members'), safe=False)
 
     elif request.method == 'DELETE':
         room.delete()
@@ -59,7 +58,7 @@ def room_detail(request, room_id):
         return HttpResponseNotAllowed(['GET', 'DELETE'])
 
 
-def room_users(request, room_id):
+def room_members(request, room_id):
     if not request.user.is_authenticated():
         return HttpResponse(status=401)
 
@@ -70,7 +69,7 @@ def room_users(request, room_id):
         return HttpResponseNotFound()
 
     if request.method == 'GET':
-        return JsonResponse(list(room.users.all().values()))
+        return JsonResponse(list(room.members.all().values('id')), safe=False)
     else:
         return HttpResponseNotAllowed(['GET'])
 
