61013f4c479ecc7dcd6b3e10c3bac1a96136af8b
ktaebum <phya.ktaebum@gmail.com>
2017-11-13 21:28:50 +0900

    some formatting
    some formatting commit

--- frontend/src/app/services/meet.service.ts
+++ frontend/src/app/services/meet.service.ts
@@ -14,7 +14,7 @@ import {UserInfo} from "../models/user-info";
 
 
 function handleError(error: any) {
-  console.error('An error occured: ', error);
+  console.error('An error occurred: ', error);
   return Promise.reject(error.message || error);
 }
 
