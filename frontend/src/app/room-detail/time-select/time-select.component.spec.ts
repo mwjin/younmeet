df5db222f0f41c4369305d8f3572b81441426206
KIM TAE BUM <phya.ktaebum@gmail.com>
2017-11-19 14:13:08 +0900

    freetime service spec create resolved

--- frontend/src/app/room-detail/time-select/time-select.component.spec.ts
+++ frontend/src/app/room-detail/time-select/time-select.component.spec.ts
@@ -5,6 +5,10 @@ import { CalendarModule } from 'fullcalendar-ag4';
 import { APP_BASE_HREF, Location, PathLocationStrategy } from '@angular/common';
 import { LocationStrategy } from '@angular/common';
 import * as $ from 'jquery';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FreetimeService } from '../../services/freetime.service';
+import { HttpModule } from '@angular/http';
+import { MeetService } from '../../services/meet.service';
 
 describe('TimeSelectComponent', () => {
   let component: TimeSelectComponent;
@@ -12,9 +16,13 @@ describe('TimeSelectComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports : [ CalendarModule.forRoot() ],
+      imports : [ CalendarModule.forRoot(),
+        RouterTestingModule,
+        HttpModule ],
       declarations : [ TimeSelectComponent ],
       providers : [ Location,
+        FreetimeService,
+        MeetService,
         { provide : LocationStrategy, useClass : PathLocationStrategy },
         { provide : APP_BASE_HREF, useValue : '/' } ]
     }).compileComponents();
