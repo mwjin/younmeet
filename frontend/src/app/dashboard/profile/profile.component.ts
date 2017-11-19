3f1333f21959b23f3bb436da65ba0699a9b44e37
KIM TAE BUM <phya.ktaebum@gmail.com>
2017-11-19 16:13:01 +0900

    profile oninit

--- frontend/src/app/dashboard/profile/profile.component.ts
+++ frontend/src/app/dashboard/profile/profile.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormGroup } from '@angular/forms';
+import { AccountService } from '../../services/account.service';
+import { User } from '../../models/user';
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  selector : 'app-profile',
+  templateUrl : './profile.component.html',
+  styleUrls : [ './profile.component.css' ]
 })
 export class ProfileComponent implements OnInit {
+  private currentUser: User;
+  profilePasswordForm: FormGroup;
+  private password: AbstractControl;
+  private passwordComfirmation: AbstractControl;
 
-  constructor() { }
+  constructor(private accountService: AccountService) {
+  }
 
   ngOnInit() {
+    this.accountService.getUserDetail()
+      .then(user => {
+        console.log(user);
+        this.currentUser = user;
+      });
   }
 
 }
