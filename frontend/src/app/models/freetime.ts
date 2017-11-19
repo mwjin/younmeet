6b6252384270dca0b06bc75e769e9860960d4ba6
KIM TAE BUM <phya.ktaebum@gmail.com>
2017-11-19 13:49:09 +0900

    delete toJson() in freetime model

--- frontend/src/app/models/freetime.ts
+++ frontend/src/app/models/freetime.ts
@@ -2,11 +2,4 @@ export class Freetime {
   constructor(public start: Date,
               public end: Date) {
   }
-
-  public toJson(): string {
-    return JSON.stringify({
-      start : this.start.toJSON(),
-      end : this.end.toJSON()
-    });
-  }
 }
