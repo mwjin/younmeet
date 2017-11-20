921f6a26b14a33f0ed5cf7408ac11f1678671c7b
KIM TAE BUM <phya.ktaebum@gmail.com>
2017-11-20 22:59:04 +0900

    add tests for freetime service

--- frontend/src/app/room-detail/time-select/time-select.component.spec.ts
+++ frontend/src/app/room-detail/time-select/time-select.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
 
 import { TimeSelectComponent } from './time-select.component';
 import { CalendarModule } from 'fullcalendar-ag4';
@@ -10,6 +10,27 @@ import { HttpModule } from '@angular/http';
 import { MeetService } from '../../services/meet.service';
 import { MeetServiceSpy } from '../../services/meet.service.spy';
 import { FreetimeServiceSpy } from '../../services/freetime.service.spy';
+import * as $ from 'jquery';
+
+const fakeCalendarEvents: Object[] = [
+  {
+    start : '2017-11-10T14:00:00Z',
+    end : '2017-11-10T14:50:00Z'
+  },
+  {
+    start : '2017-11-11T15:00:00Z',
+    end : '2017-11-11T15:50:00Z'
+  },
+  {
+    start : '2017-11-12T16:00:00Z',
+    end : '2017-11-12T17:50:00Z'
+  },
+  {
+    start : '2017-11-13T17:00:00Z',
+    end : '2017-11-13T18:30:00Z'
+  },
+];
+
 
 describe('TimeSelectComponent', () => {
   let component: TimeSelectComponent;
@@ -38,9 +59,14 @@ describe('TimeSelectComponent', () => {
     meetServiceSpy = fixture.debugElement.injector.get(MeetService) as any;
     freetimeServiceSpy = fixture.debugElement.injector.get(FreetimeService) as any;
     fixture.detectChanges();
+
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+    });
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
--- frontend/src/app/services/freetime.service.ts
+++ frontend/src/app/services/freetime.service.ts
@@ -14,27 +14,6 @@ function handleError(error: any) {
 export class FreetimeService {
   constructor(private http: Http) {}
 
-  /*
-  getFreeTimes(id: number): Promise<Freetime[]> {
-    console.log('function call');
-    return this.http.get(`api/rooms/${id}/free-times`)
-      .toPromise()
-      .then(response => {
-        let freeTimesList: Freetime[] = [];
-        let freeTimeJsonData = response.json() as FreetimeResponseData;
-        console.log('json data' + freeTimeJsonData);
-        for (let index in freeTimeJsonData) {
-          console.log('index data = ' + freeTimeJsonData[ index ]);
-          freeTimesList.push(new Freetime(freeTimeJsonData[ index ][ 'start' ], freeTimeJsonData[ index ][ 'end' ]));
-        }
-        return freeTimesList;
-      })
-      .catch(handleError);
-
-  }
-  */
-
-
   getFreeTimes(id: number): Promise<FreetimeResponseData[]> {
     return this.http.get(`api/rooms/${id}/free-times`)
       .toPromise()
@@ -42,27 +21,6 @@ export class FreetimeService {
         console.log(response.json());
         return response.json() as FreetimeResponseData[];
       })
-      /*
-      console.log(stringfiedFreeTimes);
-      for (let index in stringfiedFreeTimes) {
-        console.log((stringfiedFreeTimes[ index ][ 'start' ].toJSON()));
-        console.log(new Date('2017-11-22T00:30:00.000Z'));
-        console.log(new Date(stringfiedFreeTimes[ index ][ 'start' ].toJSON()));
-        console.log(new Date(stringfiedFreeTimes[ index ][ 'end' ].toJSON()));
-        console.log(new Freetime(new Date(stringfiedFreeTimes[ index ][ 'start' ].toJSON()),
-          new Date(stringfiedFreeTimes[ index ][ 'end' ].toJSON())));
-        let freetime: Freetime = new Freetime(new Date(stringfiedFreeTimes[ index ][ 'start' ].toJSON()),
-          new Date(stringfiedFreeTimes[ index ][ 'end' ].toJSON()));
-        freeTimeResult.push(freetime);
-        console.log(freetime);
-      }
-      console.log(freeTimeResult);
-      return stringfiedFreeTimes;
-    })
-    .then(freeTimes => {
-      console.log(freeTimes);
-      return freeTimes;
-    })*/
       .catch(handleError);
   }
 
