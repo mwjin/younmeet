dc8e57c847395a9d82bda82844b1935fbdd7e3ce
DS-Ubuntu <96lives@gmail.com>
2017-12-17 18:40:37 +0900

    Modified css

--- frontend/src/app/not-found/not-found.component.ts
+++ frontend/src/app/not-found/not-found.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styles: []
+  styleUrls: ['./not-found.component.css'],
+
 })
 export class NotFoundComponent implements OnInit {
 
